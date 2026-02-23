bab153fe3c3caf13a3a287a7866a85e8122522f3
GuilhermeAugustoFigueiredo <guilherme.figueiredo@sptech.school>
2026-02-23 18:40:32 -0300

    feat: add 3 buckets s3

--- main.tf
+++ main.tf
@@ -180,3 +180,18 @@ resource "aws_lb_target_group_attachment" "pri2" {
   port             = 80
 }
 
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "brinks-bucket-1"
+  tags = { Name = "MyS3Bucket" }
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "brinks-bucket-2"
+  tags = { Name = "MyS3Bucket" }
+}
+
+resource "aws_s3_bucket" "bucket3" {
+  bucket = "brinks-bucket-3"  
+  tags = { Name = "MyS3Bucket" }
+}
+  
